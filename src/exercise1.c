9c477473703e4ef660a42fa9b4e2365d92537514
Деменко Вячеслав Павлович ИП-513 <demenko_slava@mail.ru>
2025-10-05 20:04:38 +0700

    9popitka

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-int main(int argc, char** argv) {
-    double array[10];
-    double sum = 0;
-    for(int i = 0; i < 10; ++i) {
+
+int main() {
+    double array[10], sum = 0;
+    
+    for(int i = 0; i < 10; i++) {
         scanf("%lf", &array[i]);
-        sum +=array[i];
- }
-    double ot = (sum/10);
-    if (ot>0){
-        ot = ((sum/10)*100+0.5)/100;
+        sum += array[i];
     }
-    else{
-        ot = ((sum/10)*100-0.5)/100;
+    double average = sum / 10;
+    double rounded;
+    if (average >= 0) {
+        rounded = (long long)(average * 100 + 0.5) / 100.0;
+    } else {
+        rounded = (long long)(average * 100 - 0.5) / 100.0;
     }
-    printf("%.2f", ot);
+    
+    printf("%.2f", rounded);
     return 0;
 }
