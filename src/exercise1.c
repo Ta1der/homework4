ee1ecd642b7b6ad1984fde9db0cf246976579911
Деменко Вячеслав Павлович ИП-513 <demenko_slava@mail.ru>
2025-10-05 20:36:50 +0700

    15popitka

--- src/exercise1.c
+++ src/exercise1.c
@@ -2,14 +2,20 @@
 
 int main() {
     double array[10], sum = 0;
-    
     for(int i = 0; i < 10; i++) {
         scanf("%lf", &array[i]);
         sum += array[i];
     }
-    
     double average = sum / 10;
-    printf("%.2f\n", average);
-    
+    long long cents = (long long)(average * 1000 + 0.5);
+    if (cents % 10 >= 5) {
+        cents = cents / 10 + 1;
+    } 
+    else {
+        cents = cents / 10;
+    }
+
+    double rounded = cents / 100.0;
+    printf("%.2f\n", rounded);
     return 0;
 }
