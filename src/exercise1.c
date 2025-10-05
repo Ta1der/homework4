04a1ee8440d7970fc3242d7b4f5ce82b9c341852
Деменко Вячеслав Павлович ИП-513 <demenko_slava@mail.ru>
2025-10-05 18:09:03 +0700

    1dz

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
 int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+ int array[5];
+ printf("Введите пять целых чисел:\n");
+ for(int i = 0; i < 5; ++i) {
+ scanf("%d", &array[i]);
+ }
+ return 0;
 }
