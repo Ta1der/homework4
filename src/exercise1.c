083acff4646fd28c315711e203a775dbe8357c05
Деменко Вячеслав Павлович ИП-513 <demenko_slava@mail.ru>
2025-10-05 19:55:10 +0700

    7popitka

--- src/exercise1.c
+++ src/exercise1.c
@@ -6,7 +6,13 @@ int main(int argc, char** argv) {
         scanf("%f", &array[i]);
         sum +=array[i];
  }
-    float ot = ((sum/10)*100+0.5)/100;
+    float ot = (sum/10);
+    if (ot>0){
+        ot = ((sum/10)*100+0.5)/100;
+    }
+    else{
+        ot = ((sum/10)*100-0.5)/100;
+    }
     printf("%.2f", ot);
     return 0;
 }
