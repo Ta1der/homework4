5051f7c762879052ac43ddf165d74a6c1cacf8d3
Деменко Вячеслав Павлович ИП-513 <demenko_slava@mail.ru>
2025-10-05 19:47:34 +0700

    5popitka

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(int argc, char** argv) {
-    double array[10];
-    double sum = 0;
+    float array[10];
+    float sum = 0;
     for(int i = 0; i < 10; ++i) {
-        scanf("%lf", &array[i]);
+        scanf("%f", &array[i]);
         sum +=array[i];
  }
-    double ot = sum/10;
-    printf("%.2f", ot + 0.0000001);
+    float ot = sum/10;
+    printf("%.2f", ot);
     return 0;
 }
