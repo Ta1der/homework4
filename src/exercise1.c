ca99f0e7d1417f3ca722a223f3d9e9304dcb5f75
Деменко Вячеслав Павлович ИП-513 <demenko_slava@mail.ru>
2025-10-05 20:20:39 +0700

    12popitka

--- src/exercise1.c
+++ src/exercise1.c
@@ -2,16 +2,12 @@
 int main(int argc, char** argv) {
     double array[10];
     double sum = 0;
-    int t = 1;
     for(int i = 0; i < 10; ++i) {
         scanf("%lf", &array[i]);
         sum +=array[i];
  }
     double ot = (sum/10);
-    if(ot*1000>=5){
-        ot+=0.01;
-        t +=1;
-    }
-    printf("%.2f", ot);
+    double rounded = (int)(ot * 100 + 0.5) / 100.0;
+    printf("%.2f", rounded);
     return 0;
 }
